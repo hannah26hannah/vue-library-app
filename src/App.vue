e16776499bda50391aea84d2373c0c5eae4a7b83
hannah26hannah <ooyoonoo24@gmail.com>
2021-01-11 16:19:00 +0900

    :bento: add footer

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,20 @@
     <the-header />
     <router-view class="router-section" />
     <ScrollUp :scroll-duration="1000" :scroll-y="250" />
+    <the-footer />
   </div>
 </template>
 
 <script>
 import theHeader from "@/components/header.vue";
+import theFooter from "@/components/footer.vue";
 import ScrollUp from "vue-scroll-up";
 
 export default {
   components: {
     theHeader,
-    ScrollUp
+    ScrollUp,
+    theFooter
   }
 };
 </script>
---
+++ src/components/footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-footer class="el-footer">
+    <ul class="company-desc">
+      <li>
+        <router-link to="/">
+          <img class="logo" src="@/assets/img/logo.png" alt="週間讀書" />
+        </router-link>
+      </li>
+      <li>@2021 週間讀書</li>
+    </ul>
+    <section class="site-detail">
+      <ul>
+        <li>
+          <router-link to="/challenge">Challenge</router-link>
+        </li>
+        <li>
+          <router-link to="/book">Book</router-link>
+        </li>
+        <li>
+          <router-link to="/timeline">Timeline</router-link>
+        </li>
+      </ul>
+      <ul>
+        <li>logo</li>
+        <li>logo</li>
+        <li>logo</li>
+      </ul>
+    </section>
+  </el-footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
