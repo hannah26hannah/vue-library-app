39d49c4edfd06fcdc31b169867de2f6923724cc5
hannah26hannah <ooyoonoo24@gmail.com>
2021-01-12 23:47:52 +0900

    :lock: update security Firebase rules (firestore, storage)

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,8 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
+import "firebase/firebase-database";
+
 import store from "./store";
 
 const firebaseConfig = {
