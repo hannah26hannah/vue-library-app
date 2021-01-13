727179ea54641f4663ebcd702494ed5a7474b8e2
hannah26hannah <ooyoonoo24@gmail.com>
2021-01-14 00:54:59 +0900

    :construction: Deployed new index for Algolia : to be fixed soon

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,38 @@
+    
+const admin = require("firebase-admin");
 const functions = require("firebase-functions");
+const algoliasearch = require("algoliasearch");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+const db = admin.firestore();
+
+const algoliaClient = algoliasearch(functions.config().algolia.appid, functions.config().algolia.apikey);
+
+const collectionIndexName = "reviews";
+const collectionIndex = algoliaClient.initIndex(collectionIndexName);
+
+exports.sendCollectionToAlgolia = functions.https.onRequest(async (req, res) => {
+    const algoliaRecords = [];
+    const bookRecordRef = db.collection("bookRecord");
+    const querySnapshot = await bookRecordRef.doc({userUID}).collection("bookInfo").get();
+
+    querySnapshot.docs.forEach(doc => {
+        const document = doc.data();
+
+        const record = {
+            objectId: doc.id,
+            created: document.created,
+            genre: document.genre,
+            quote: document.quote,
+            review: document.review,
+            title: document.title,
+            writer: document.writer
+        };
+
+        algoliaRecords.push(record);
+    });
+
+    collectionIndex.saveObjects(algoliaRecords, (_error, content) => {
+        res.status(200).send("bookInfo Collection was indexed to Algolria successfully");
+    })
+})
